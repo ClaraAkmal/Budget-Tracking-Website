d23da08061f9968d43c6687b224f645774441cfd
Clara <claraakmalwadie2004@gmail.com>
2026-04-19 19:14:54 +0200

    Full System works

--- budget-tracker/src/app/shared/shared-module.ts
+++ budget-tracker/src/app/shared/shared-module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar';
 import { Spinner } from './spinner/spinner';
 import { CurrencyFormatPipe } from './pipes/currency-format.pipe';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent, Spinner, CurrencyFormatPipe],
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   exports: [NavbarComponent, Spinner, CurrencyFormatPipe]
 })
 export class SharedModule {}
